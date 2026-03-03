9886bcc9d50eae40e061eb9347cf7d5345ae2749
Charles A Taylor <c.angustaylor@gmail.com>
2026-03-03 02:11:35 -0500

    Reorganize Edit menu: Anchors submenu, Paste Multiple at Edit level

--- paste_hidden/menu.py
+++ paste_hidden/menu.py
@@ -4,22 +4,42 @@ import anchor
 import labels
 
 menu = nuke.menu("Nuke")
-menu.addCommand("Edit/Copy", "paste_hidden.copy_hidden()", "^C")
-menu.addCommand("Edit/Cut", "paste_hidden.cut_hidden()", "^X")
+edit_menu = menu.findItem("Edit")
+
+menu.addCommand("Edit/Copy",  "paste_hidden.copy_hidden()",  "^C")
+menu.addCommand("Edit/Cut",   "paste_hidden.cut_hidden()",   "^X")
 menu.addCommand("Edit/Paste", "paste_hidden.paste_hidden()", "^V")
-menu.addCommand("Edit/Paste Multiple", "paste_hidden.paste_multiple_hidden()", "+^V")
 
-menu.addCommand("Edit/Copy (old)", "paste_hidden.copy_old()")
-menu.addCommand("Edit/Cut (old)", "paste_hidden.cut_old()")
-menu.addCommand("Edit/Paste (old)", "paste_hidden.paste_old()", "+^D")
+def _find_item_index(parent_menu, item_name):
+    for position, menu_item in enumerate(parent_menu.items()):
+        if menu_item.name() == item_name:
+            return position
+    return -1
 
-menu.addCommand("Edit/Create Anchor",      "anchor.create_anchor()")
-menu.addCommand("Edit/Rename Anchor",      "anchor.rename_selected_anchor()")
-menu.addCommand("Edit/Create Link",        "anchor.select_anchor_and_create()")
-menu.addCommand("Edit/Anchor",             "anchor.anchor_shortcut()", "A")
-menu.addCommand("Edit/Reconnect All Links","anchor.reconnect_all_links()")
-menu.addCommand("Edit/Anchor Find",        "anchor.select_anchor_and_navigate()", "alt+A")
+paste_index = _find_item_index(edit_menu, "Paste")
+edit_menu.addCommand(
+    "Paste Multiple",
+    "paste_hidden.paste_multiple_hidden()",
+    index=paste_index + 1,
+)
 
-menu.addCommand("Edit/Label (Large)",  "labels.create_large_label()",  "+M")
-menu.addCommand("Edit/Label (Medium)", "labels.create_medium_label()", "+N")
-menu.addCommand("Edit/Append Label",   "labels.append_to_label()",     "^M")
+anchors_menu = edit_menu.addMenu("Anchors")
+
+anchors_menu.addCommand("Create Anchor",       "anchor.create_anchor()")
+anchors_menu.addCommand("Rename Anchor",       "anchor.rename_selected_anchor()")
+anchors_menu.addCommand("Create Link",         "anchor.select_anchor_and_create()")
+anchors_menu.addCommand("Anchor",              "anchor.anchor_shortcut()", "A")
+anchors_menu.addCommand("Reconnect All Links", "anchor.reconnect_all_links()")
+anchors_menu.addCommand("Anchor Find",         "anchor.select_anchor_and_navigate()", "alt+A")
+
+anchors_menu.addSeparator()
+
+anchors_menu.addCommand("Label (Large)",  "labels.create_large_label()",  "+M")
+anchors_menu.addCommand("Label (Medium)", "labels.create_medium_label()", "+N")
+anchors_menu.addCommand("Append Label",   "labels.append_to_label()",     "^M")
+
+anchors_menu.addSeparator()
+
+anchors_menu.addCommand("Copy (old)",  "paste_hidden.copy_old()")
+anchors_menu.addCommand("Cut (old)",   "paste_hidden.cut_old()")
+anchors_menu.addCommand("Paste (old)", "paste_hidden.paste_old()", "+^D")
